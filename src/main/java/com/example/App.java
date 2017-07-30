cbeafb4c205859d2c4c55fa0e6c2c66d20fd254d
Michael Easter <codetojoy@gmail.com>
2017-07-30 17:24:09 -0300

    commit A

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -5,6 +5,6 @@ public class App {
     private static final String VERSION_INFO = "V 1.0.0 master";
 
     public static void main(String[] args) {
-        System.out.println("TRACER hello from app. version: " + VERSION_INFO);
+        System.out.println("TRACER hello [A] from app. version: " + VERSION_INFO);
     }
 }
